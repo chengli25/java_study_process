44ce7feddae9dc8b22cf7342219b3b60837c0094
TIMI-TI <1444171773@qq.com>
2020-11-29 16:27:23 +0800

    11.29 16:27

--- network_program/src/web_char_room/Client.java
+++ network_program/src/web_char_room/Client.java
@@ -9,7 +9,7 @@ import java.net.Socket;
  */
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket s = new Socket("localhost",9995);
+        Socket s = new Socket("localhost",9999);
 
         //启动两个线程，一个接受信息，一个发送信息，不能重复
         ResThread mt = new ResThread(s,"服务器");
--- network_program/src/web_char_room/Server.java
+++ network_program/src/web_char_room/Server.java
@@ -10,11 +10,12 @@ import java.net.Socket;
  */
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket(9995);
+        ServerSocket ss = new ServerSocket(9999);
         Socket s = ss.accept();
 
         //启动两个线程，一个接受信息，一个发送信息，不能重复
         ResThread mt = new ResThread(s,"客户端");
+
         mt.start();
 
         SendThread st = new SendThread(s);
